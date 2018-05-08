62a43fa30bca4d5d68f4fb88a4408f1cd9e92314
mac <mac@macdeMacBook-Pro.local>
2018-05-09 06:18:14 +0800

    exports modify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = module.exports.default = factory() :
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global.ES6Promise = factory());
 }(this, (function () { 'use strict';
